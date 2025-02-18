23bf37fe0066df73603aae16c44b18fdde86d75a
Andriana Syvanych <asyvanych@gmail.com>
2025-02-18 10:30:54 +0200

    Added JS to html and jokes file

---
+++ jokes.js
@@ -0,0 +1,23 @@
+// Test for jokes
+var joke = "JavaScript walked into a bar....";
+var toldJoke = false;
+var punchLine = "Better watch out for those semi-colons";
+var percentage = 20;
+var result
+
+if (toldJoke == true) {
+    alert(punchLine);
+} else {
+    alert(joke);
+}
+
+// Movie Night
+var zipCode = 98104;
+var joesFavouriteMovie = "Forbidden Planet";
+var movieTicket$   =   9;
+
+if (movieTicket$ >= 9) {
+    alert("Too much!");
+} else {
+    alert("We're giong to see " + joesFavouriteMovie);
+}
