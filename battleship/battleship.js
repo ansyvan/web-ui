5d794c8d3e11eac04ec81af8bfd890410ba51288
Andriana Syvanych <asyvanych@gmail.com>
2025-02-28 10:24:34 +0200

    Updated battleship game

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -8,4 +8,26 @@ var isSunk = false;
 
 while (isSunk == false) {
     guess = prompt("Ready, aim, fire! (enter a number from 0 to 6):");
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+        
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            alert("HIT!");
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        } else {
+            alert("MISS");
+            guesses = guesses + 1;
+        }
+    }
 }
+
+var stats = "Tou took " + guesses + " guesses to sink the battleship, " +
+            "which means your shooting accuracy was " + (3/guesses);
+
+alert(stats);
